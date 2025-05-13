9db358b137badfdb89a8c93a731bd906a1055a64
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-05-13 16:30:28 +0000

    Updates

---
+++ Kickstarter.7db12d41.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("backToTop");window.addEventListener("scroll",()=>{e.style.display=window.scrollY>300?"block":"none"}),e.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})})});
+//# sourceMappingURL=Kickstarter.7db12d41.js.map
---
+++ Kickstarter.8fd3aff4.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("backToTop");window.addEventListener("scroll",()=>{window.scrollY>300?e.style.display="block":e.style.display="none"}),e.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})})});
+//# sourceMappingURL=Kickstarter.8fd3aff4.js.map
